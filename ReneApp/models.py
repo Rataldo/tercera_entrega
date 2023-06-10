a26c3730c2dcfadc4f102d9585b437bce42f7a16
Rene <aranzaezrene@gmail.com>
2023-06-10 17:19:42 -0300

    second commit tercera entrega

--- ReneApp/models.py
+++ ReneApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40)
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40)
